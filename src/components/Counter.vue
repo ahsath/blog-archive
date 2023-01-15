eb29f2536076f7583b54a552df2dd1fa357a608e
Ahsath <ahsath@gmail.com>
2023-01-15 14:54:29 +0000

    add Counter.vue component

---
+++ src/components/Counter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>{{ count }}</h1>
+    <button @click="count++">Increment</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const props = withDefaults(defineProps<{ count?: number }>(), { count: 0 });
+
+const count = ref(props.count);
+</script>
